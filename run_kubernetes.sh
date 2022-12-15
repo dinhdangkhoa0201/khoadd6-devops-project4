64747c70c8d073ddd23eb1b73ac46c1e2b0cf758
KhoaDD6 <dinhdangkhoa0201@gmail.com>
2022-12-15 22:59:36 +0700

    Update

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # This is your Docker ID/path
- dockerpath=dinhdangkhoa0201/khoadd6-devops-project4-api
+dockerpath=dinhdangkhoa0201/khoadd6-devops-project4-api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
